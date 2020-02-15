5f6ca8cdb8afe82a38bc9280e0b236f18d0bc04e
QichaoGuo <2447372346@qq.com>
2020-02-15 18:26:22 +0800

    初始化

--- MCS.Web/src/App.vue
+++ MCS.Web/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FileList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const FileList = () => import('./components/FileList.vue')
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FileList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ MCS.Web/src/components/FileList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+        <li>
+            file01
+        </li>
+        <li>
+            file02
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileList'
+}
+</script>
+
+<style scoped>
+</style>
